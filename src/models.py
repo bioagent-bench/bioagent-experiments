29505fb01c2fe8199a345555cb822bcf34fde08e
Dionizije Fa <dionizije.fa@hotmail.com>
2025-12-06 22:58:47 +0100

    Enable all models

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 MODELS = [
-    # "gpt-5-1-codex",
-    # "mistral-large-3",
-    # "gpt-5-1",
-    # "gpt-oss-120b",
+    "gpt-5-1-codex",
+    "mistral-large-3",
+    "gpt-5-1",
+    "gpt-oss-120b",
     "claude-opus-4-5",
     "claude-sonnet-4-5", 
     "gpt-5-1-codex-max",
